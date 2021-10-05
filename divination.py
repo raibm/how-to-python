72ce62e93a156e441b8124d478683a04d2a5ed59
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-10-04 22:18:59 -0300

    Python3 -p1a3- Elif

--- divination.py
+++ divination.py
@@ -5,10 +5,15 @@ print("*********************************")
 secret_number = 42
 
 user_number = int(input("Try a number: "))
+print("\n")
 
-if (secret_number == user_number):
+got_it = secret_number == user_number;
+is_smaller_than = user_number < secret_number;
+
+if (got_it):
     print("U got it!")
+elif (is_smaller_than):
+    print("Bad choise, your try is small than secret number.")
 else:
-    print("Bad choise...")
-
+    print("Ops, your number is so big, try another number...")
 print("Bye!")
