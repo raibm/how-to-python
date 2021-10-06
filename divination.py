f68c54b2c65b6d016e6d6ae5aac94d71e78c1f29
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-10-06 20:54:38 -0300

    Python3 -p1a4- While

--- divination.py
+++ divination.py
@@ -3,17 +3,22 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 secret_number = 42
+total_trys = 3
+round = 1
 
-user_number = int(input("Try a number: "))
-print("\n")
+while (round <= total_trys):
+    print("Try number", round, "of", total_trys)
+    user_number = int(input("Try a number: "))
+    print("\n")
 
-got_it = secret_number == user_number;
-is_smaller_than = user_number < secret_number;
+    got_it = secret_number == user_number;
+    is_smaller_than = user_number < secret_number;
 
-if (got_it):
-    print("U got it!")
-elif (is_smaller_than):
-    print("Bad choise, your try is small than secret number.")
-else:
-    print("Ops, your number is so big, try another number...")
+    if (got_it):
+        print("U got it!")
+    elif (is_smaller_than):
+        print("Bad choise, your try is small than secret number.")
+    else:
+        print("Ops, your number is so big, try another number...")
+    round += 1
 print("Bye!")
