6ec022cf927afd19683d965d4f89b62e382528f3
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-10-08 20:04:02 -0300

    Python3 -p1a5 - Break, continue, or

---
+++ string_format.py
@@ -0,0 +1,10 @@
+print("Test string, here is number {}, and here a number {}".format(2, 42))
+print("Test, monetary value is U$ {:f}".format(43.20))
+print("Test, monetary value is U$ {:.2f}".format(43.20))
+print("Test, monetary value is U$ {:7.2f}".format(43.20))
+print("Test, monetary value is U$ {:07.1f}".format(43.20))
+print("Test, int value is U$ {:07d}".format(43))
+print("Test, date format {:2d}/{:2d}".format(9,4))
+print("Test, date format {:02d}/{:02d}".format(9,4))
+print("Test, date format {:02d}/{:02d}".format(11,11))
+
