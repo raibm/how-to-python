780cfb7ece837dfe34a191f79ea86db8265ff9bf
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-10-21 20:18:42 -0300

    Python3 -p2a5 - Fix somethings and adding some conditionals to validate the guess of user

--- hang.py
+++ hang.py
@@ -3,24 +3,31 @@ def play():
     print("Welcome to the gibble game!")
     print("*********************************")
 
-    secret_word = "banana"
+    secret_word = "banana".lower()
     right_letters = ["_", "_", "_", "_", "_", "_"]
 
     got_it = False
     hanged = False
+    wrongs_letters = 0
 
     print(right_letters)
 
     while (not got_it and not hanged):
         guess = input("Try a letter")
-        guess = guess.strip()
+        guess = guess.strip().lower()
         index = 0
 
-        for letter in secret_word:
-            if (guess.lower() == letter.lower()):
-                right_letters[index] = letter
-            index = index + 1
-        print(right_letters)
+        if (guess in secret_word and wrongs_letters < len(secret_word)):
+            for letter in secret_word:
+                if (guess == letter):
+                    right_letters[index] = letter
+                index = index + 1
+        else:
+            wrongs_letters += 1
+            hanged = wrongs_letters == len(secret_word)
+            got_it = "_" not in right_letters
+            print("errrrouuu: ",wrongs_letters)
+
 
 if (__name__ == "__main__"):
     play()
